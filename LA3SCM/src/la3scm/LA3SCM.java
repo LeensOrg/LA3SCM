94ed971034ff82850840368a7577d5ec4350a258
leen297 <g00057106@aus.edu>
2017-02-18 17:57:36 +0400

    Bug found, member A

--- LA3SCM/src/la3scm/LA3SCM.java
+++ LA3SCM/src/la3scm/LA3SCM.java
@@ -16,6 +16,7 @@ public class LA3SCM {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //bug found
 <<<<<<< HEAD
         System.out.println("Hello World!");
 =======
