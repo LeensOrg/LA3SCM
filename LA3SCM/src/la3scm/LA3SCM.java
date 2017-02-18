a2984b8b02b87fc5f0f2d0ad36327b32510f7f10
leen297 <g00057106@aus.edu>
2017-02-18 16:27:56 +0400

    Feature B added

--- LA3SCM/src/la3scm/LA3SCM.java
+++ LA3SCM/src/la3scm/LA3SCM.java
@@ -16,6 +16,10 @@ public class LA3SCM {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World");
     }
-    
+    void addFeatureB()
+    {
+        System.out.println("Added feature B");
+    }
 }
